2bcf2b760afd85eb13c5de7bff25c85422a3443d
johnsonlee <g.johnsonlee@gmail.com>
2020-06-13 09:40:17 +0800

    Release version 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "io.johnsonlee.booster"
-version = "1.7.2"
+version = "2.0.0"
 
 
 repositories {
@@ -25,8 +25,8 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.google.auto.service:auto-service:1.0-rc4")
-    implementation("com.didiglobal.booster:booster-command:1.7.2")
-    implementation("com.didiglobal.booster:booster-kotlinx:1.7.2")
+    implementation("com.didiglobal.booster:booster-command:2.0.0")
+    implementation("com.didiglobal.booster:booster-kotlinx:2.0.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
